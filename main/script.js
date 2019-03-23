688193de8341d4efb8f1a880afd1a0af12aee2b7
dan jugz <njugunadaniel364@gmail.com>
2019-03-23 16:46:29 +0300

    business logic complete

--- main/script.js
+++ main/script.js
@@ -1,4 +1,3 @@
-
 function  tracker(){
   /*USER INTERFACE LOGIC*/
 
@@ -10,3 +9,31 @@ function  tracker(){
   var sidec=parseInt(collect3);
   var sides=[sidea,sideb,sidec];
   var type=["makes an equilateral triangle","makes an isosceles triangle","makes a scalene triangle","doesn't make a triangle"]
+
+    /*BUSINESS LOGIC*/
+
+    if(sides[0] <=0 || sides[1] <=0 || sides[2] <=0){
+      var firstOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
+      document.getElementById("trackOutput").innerHTML=firstOutput;
+    }
+    else if(sides[0]===sides[1] && sides[1]===sides[2] && sides[2]===sides[0]){
+      var secondOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[0];
+      document.getElementById("trackOutput").innerHTML=secondOutput;
+    }
+    else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
+      var thirdOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[1];
+      document.getElementById("trackOutput").innerHTML=thirdOutput;
+    }
+    else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[2]!==sides[1] && sides[0]+sides[1]>sides[2] && sides[1]+sides[2]>sides[0] && sides[2]+sides[0]>sides[1]){
+      var fourthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[2];
+      document.getElementById("trackOutput").innerHTML=fourthOutput;
+    }
+    else if(sides[0]+sides[1]<sides[2] || sides[1]+sides[2]<sides[0] && sides[2]+sides[0]<sides[1]){
+      var fifthOutput=sides[0]+" "+sides[1]+" "+sides[2]+" "+type[3];
+      document.getElementById("trackOutput").innerHTML=fifthOutput;
+    }
+    else {
+      var sixthOutput=type[3];
+      document.getElementById("trackOutput").innerHTML=sixthOutput;
+    }
+  }
